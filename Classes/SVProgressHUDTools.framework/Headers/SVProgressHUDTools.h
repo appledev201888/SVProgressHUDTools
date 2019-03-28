a1904ba8f9d1b732d05f49bf610052f874f41221
appledev201888 <2779461804@qq.com>
2019-03-28 17:44:38 +0800

    提交的描述说明

--- Classes/SVProgressHUDTools.framework/Headers/SVProgressHUDTools.h
+++ Classes/SVProgressHUDTools.framework/Headers/SVProgressHUDTools.h
@@ -1,14 +1,14 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for IQKeyboardTools.
-FOUNDATION_EXPORT double IQKeyboardToolsVersionNumber;
+//! Project version number for SVProgressHUDTools.
+FOUNDATION_EXPORT double SVProgressHUDToolsVersionNumber;
 
-//! Project version string for IQKeyboardTools.
-FOUNDATION_EXPORT const unsigned char IQKeyboardToolsVersionString[];
+//! Project version string for SVProgressHUDTools.
+FOUNDATION_EXPORT const unsigned char SVProgressHUDToolsVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <IQKeyboardTools/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <SVProgressHUDTools/PublicHeader.h>
 
-#import <IQKeyboardTools/IQKeyboardToolsManage.h>
+#import <SVProgressHUDTools/SVProgressHUDToolsManage.h>
 
 
