d377ad674944487b85d56ca3677bab290242356b
appledev201888 <2779461804@qq.com>
2019-04-01 14:15:36 +0800

    提交的描述说明

---
+++ Classes/SVProgressHUDTools.framework/SVProgressHUDTools.h
@@ -0,0 +1,14 @@
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVProgressHUDTools.
+FOUNDATION_EXPORT double SVProgressHUDToolsVersionNumber;
+
+//! Project version string for SVProgressHUDTools.
+FOUNDATION_EXPORT const unsigned char SVProgressHUDToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVProgressHUDTools/PublicHeader.h>
+
+#import <SVProgressHUDTools/SVProgressHUDToolsManage.h>
+
+
